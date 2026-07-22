57eaaa0ef0294c2f8adf916e7fc53bdadb73c11b
GabrielJesus0 <gabriel053jesus@gmail.com>
2026-07-22 10:31:13 -0300

    fix: update .env to vite

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import dotenv from 'dotenv';
 dotenv.config();
 
-const baseURL = process.env.API_URL ?? "http://localhost:8000"
+const baseURL = import.meta.env.VITE_API_URL ?? "http://localhost:8000"
 
 export const api = axios.create({
     baseURL
